6eb5a2f869fb70de7242aa50ecf78656e7f53bc3
Ola Kaznowska <118020449+Ola-Kaznowska@users.noreply.github.com>
2024-10-08 10:21:48 +0200

    Add files via upload

---
+++ A program that divides numbers.py
@@ -0,0 +1,16 @@
+import os
+
+os.system("cls")
+
+
+for i in range(50):
+    
+    print("A program that divides numbers")
+
+    a = int(input("Enter the first number: "))
+    print()   
+
+    b = int(input("Enter the second digit: "))
+    print()  
+
+    print(a // b) 
---
+++ Multiplication program.py
@@ -0,0 +1,16 @@
+import os 
+
+os.system("cls")
+
+for i in range(50):
+    
+
+    print("Multiplication program")
+
+    a = int(input("Enter the first number: "))
+    print()
+
+    b = int(input("Enter the second digit: "))
+    print()
+
+    print(a * b)   
