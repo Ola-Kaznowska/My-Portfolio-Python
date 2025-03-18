b335fa02fcba58236c703f714aebb6b744a041bf
Ola Kaznowska <118020449+Ola-Kaznowska@users.noreply.github.com>
2025-03-18 20:59:45 +0100

    Add files via upload

---
+++ cs.py
@@ -0,0 +1,5 @@
+def map_name():
+    print("The dust")
+
+def buy_gun():
+    print("You have bought a gun")
---
+++ lol.py
@@ -0,0 +1,4 @@
+def map_name():
+    print("Summer's rift")
+def buy_somefing():
+    print("You have boot somefing")
