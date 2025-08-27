e7181135a1a03d6d18ef23ece2f954b3f743c89f
Ola Kaznowska <118020449+Ola-Kaznowska@users.noreply.github.com>
2025-08-27 16:40:44 +0200

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,15 @@
+My_Tuple = ("English", "Python", "Coffee", "PCEP")
+
+print(len(My_Tuple))
+
+
+
+Next_Tuples = ("Hello", "Hi")
+
+print(type(Next_Tuples))
+
+
+Tuple = ("Hello","Python")
+
+for iterating in Tuple:
+    print(My_Tuple)
